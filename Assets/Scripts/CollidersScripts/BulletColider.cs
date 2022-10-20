2fcef078561caa892d4c944c53d065b466c49b94
d2dyno1 <d2dyno1.53011783+d2dyno1@users.noreply.github.com>
2022-10-20 14:17:12 +0100

    Changed textures of enemies

--- Assets/Scripts/CollidersScripts/BulletColider.cs
+++ Assets/Scripts/CollidersScripts/BulletColider.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class BulletColider : MonoBehaviour
 {
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("Colision detected");
         Destroy(gameObject);
     }
 }
