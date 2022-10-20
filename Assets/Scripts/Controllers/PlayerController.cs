c34f056c1f0f1c6ca4db144344ecec2d7d379769
buggg0 <81020323+buggg0@users.noreply.github.com>
2022-10-20 11:24:09 +0200

    Shooting logic
    Added shooting logic and bullets

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        //rotate to pointer with RotateToPointer
+        RotateToPointer();
         _direction = RetriveMoveInput();
         transform.Translate(_direction * Time.deltaTime * _speed);
     }
@@ -17,5 +19,13 @@ public class PlayerController : MonoBehaviour
     {
         return new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
     }
-
+    
+    public void RotateToPointer()
+    {
+        Vector3 _objectPos = Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 _mousePos = Input.mousePosition;
+        _mousePos -= _objectPos;
+        float angle = Mathf.Atan2(_mousePos.y, _mousePos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
 }
--- Assets/Scripts/Controllers/ShootingController.cs
+++ Assets/Scripts/Controllers/ShootingController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class ShootingController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public GameObject ProjectilePrefab;
+    public Transform FirePoint;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
     }
+
+    void Shoot()
+    {
+        var projectile = Instantiate(ProjectilePrefab, FirePoint.position, FirePoint.rotation);
+        var rb = projectile.GetComponent<Rigidbody2D>();
+        rb.AddForce(FirePoint.up * 15, ForceMode2D.Impulse);
+    }
+
+
 }
